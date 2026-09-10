35e95bb09c0ac8e963635629c5cc69d370fc3c62
xMarianaC118x <marianacubillos13@gmail.com>
2026-09-10 11:11:45 -0500

    v1.3.30-fix: script seguro para sincronizar base de datos

--- waip.php
+++ waip.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WordPress AI Platform (WAIP)
  * Description: Motor de asistentes de IA modular y marca blanca para WordPress.
- * Version: 1.3.29
+ * Version: 1.3.30
  * Author: Mariana Cubillos
  * Text Domain: waip
  */
